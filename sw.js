21a16171bc69dd415aadc77f29b4453a7f23b2eb
JKB <bocklejoe647@gmail.com>
2026-07-21 13:11:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    ============================================================ */
 "use strict";
 
-const VERSION = "v73";
+const VERSION = "v75";
 const CACHE = "trainingsapp-" + VERSION;
 const DATEIEN = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png", "icon-180.png"];
 
